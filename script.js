09924fc99b8e6b501abcee14aa9ea64662b6c5f1
c6y <k@eboy.com>
2018-01-24 21:23:17 -0800

    separate canvases for head, eyes and nose
    This will not work if the whole image should be available for download

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function clickNose() {
   document.getElementById("demo").innerHTML = "nose clicked";
 }
 function addFoo() {
-  var canvas = document.getElementById("myCanvas");
+  var canvas = document.getElementById("headCanvas");
   var ctx = canvas.getContext("2d");
   ctx.fillStyle = "#FF0000";
-  ctx.fillRect(0, 0, 64, 64);
+  ctx.fillRect(0, 0, 256, 256);
 }
 
 function addBar() {
-  var canvas = document.getElementById("myCanvas");
+  var canvas = document.getElementById("headCanvas");
   var ctx = canvas.getContext("2d");
   ctx.fillStyle = "#00FF00";
   ctx.fillRect(128, 128, 64, 64);
@@ -21,13 +21,25 @@ function addBar() {
 function addHeadElement() {
   const img = new Image();
   img.src = event.target.src;
-  console.log('img: ' + img);
   img.onload = function() {
-    const canvas = document.getElementById("myCanvas");
+    const canvas = document.getElementById("headCanvas");
     const ctx = canvas.getContext("2d");
     ctx.mozImageSmoothingEnabled = false;
     ctx.msImageSmoothingEnabled = false;
     ctx.imageSmoothingEnabled = false;
-    ctx.drawImage(img, 0, 0);
+    ctx.drawImage(img, 0, 0, 256, 256);
+  }
+}
+
+function addNoseElement() {
+  const img = new Image();
+  img.src = event.target.src;
+  img.onload = function() {
+    const canvas = document.getElementById("noseCanvas");
+    const ctx = canvas.getContext("2d");
+    ctx.mozImageSmoothingEnabled = false;
+    ctx.msImageSmoothingEnabled = false;
+    ctx.imageSmoothingEnabled = false;
+    ctx.drawImage(img, 0, 0, 256, 256);
   }
 }
