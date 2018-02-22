515d1aca3ded8d747cbe92de5b5b667a070c113a
c6y <k@eboy.com>
2018-02-21 23:43:58 -0800

    initial 18x18 pixel setup, nose removed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const avatarW = 512; // avatar width
-const avatarH = 512; // avatar height
+const avatarW = 720; // avatar width
+const avatarH = 720; // avatar height
 const offsetV = -8;
 
 let isHead = null;
@@ -69,38 +69,6 @@ function addEyes() {
 }
 
 /**
- * add nose to nose canvas, remove previous nose
- */
-function addNose() {
-  const img = new Image();
-  img.src = event.target.src;
-  noseFile = img.src;
-  if (isNose != noseFile) {
-    // draw nose if there's no or different nose
-    img.onload = function() {
-      const canvas = document.getElementById("noseCanvas");
-      const ctx = canvas.getContext("2d");
-      ctx.mozImageSmoothingEnabled = false;
-      ctx.msImageSmoothingEnabled = false;
-      ctx.imageSmoothingEnabled = false;
-      ctx.clearRect(0, 0, avatarW, avatarH);
-      ctx.drawImage(img, 0, offsetV, avatarW, avatarH);
-      mergeCanvases();
-      isNose = noseFile;
-    }
-    setActiveClass('noseElement', 'noseElementActive');
-  } else {
-    // remove nose if same nose is on canvas
-    const canvas = document.getElementById("noseCanvas");
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, avatarW, avatarH);
-    mergeCanvases();
-    isNose = null;
-    event.target.setAttribute("class", "noseElement");
-  }
-}
-
-/**
  * add mouth to mouth canvas, remove previous mouth
  */
 function addMouth() {
@@ -138,18 +106,16 @@ function addMouth() {
 function mergeCanvases(){
   const headC = document.getElementById('headCanvas');
   const eyesC = document.getElementById('eyesCanvas');
-  const noseC = document.getElementById('noseCanvas');
   const mouthC = document.getElementById('mouthCanvas');
 
   const canvas = document.getElementById('mergedCanvas');
   const ctx = canvas.getContext('2d');
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = 'white';
+  ctx.fillStyle = '#ffda06';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(headC, 0, 0);
   ctx.drawImage(eyesC, 0, 0);
-  ctx.drawImage(noseC, 0, 0);
   ctx.drawImage(mouthC, 0, 0);
 }
 
@@ -179,16 +145,6 @@ function removeHead() {
  */
 function removeEyes() {
   const canvas = document.getElementById("eyesCanvas");
-  const ctx = canvas.getContext("2d");
-  ctx.clearRect(0, 0, avatarW, avatarH);
-  mergeCanvases();
-}
-
-/**
- * remove nose from canvas
- */
-function removeNose() {
-  const canvas = document.getElementById("noseCanvas");
   const ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, avatarW, avatarH);
   mergeCanvases();
