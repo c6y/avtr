05843193f125fe54fc88108d5b441652c8aceb9d
c6y <k@eboy.com>
2018-02-28 13:41:28 -0800

    head color changes randomly, neck now black

--- script.js
+++ script.js
@@ -1,65 +1,95 @@
+/* eslint-disable no-unused-vars*/
+
 const avatarW = 512; // avatar width
 const avatarH = 512; // avatar height
 const offsetV = -8;
 
-let isHead = null;
 let isEyes = null;
 let isNose = null;
 let isMouth = null;
 
-let neckImages = new Array();
+let pImages = [];
+
+// to preload the images, get current directory's path
+const loc = window.location.pathname;
+const dir = loc.substring(0, loc.lastIndexOf('/'));
 
 /**
  * preload neck images into array
  */
 function preloadimages() {
-  for (i = 0; i < preloadimages.arguments.length; i++){
-    neckImages[i] = new Image();
-    neckImages[i].src = preloadimages.arguments[i];
+  let i = 0;
+  for (i = 0; i < preloadimages.arguments.length; i++) {
+    pImages[i] = new Image();
+    pImages[i].src = dir + preloadimages.arguments[i];
   }
 }
 
-// preload neck shape images
+// preload neck and head shape images
 preloadimages(
-  "/media/neck/neck-001.svg",
-  "/media/neck/neck-002.svg",
-  "/media/neck/neck-003.svg",
-  "/media/neck/neck-004.svg",
-  "/media/neck/neck-005.svg",
-  "/media/neck/neck-006.svg",
-  "/media/neck/neck-007.svg",
-  "/media/neck/neck-008.svg"
+  "/media/neck/neck-001.svg", // neck
+  "/media/head/head-001.svg",
+  "/media/head/head-002.svg",
+  "/media/head/head-003.svg",
+  "/media/head/head-004.svg",
+  "/media/head/head-005.svg",
+  "/media/head/head-006.svg",
+  "/media/head/head-007.svg",
+  "/media/head/head-008.svg"
 );
+
+const colorSetA = [
+  '#4088EE',
+  '#33BB55',
+  '#FF3333',
+  '#FFDD00',
+  '#B156C4',
+  '#23D6C9',
+  '#FF0088',
+  '#BBEE33'
+];
+// const colorsSkin = [
+//   '#F8AA8F',
+//   '#e0a899',
+//   '#c99789'
+// ];
 
 /**
  * add head to head canvas, remove previous head
  */
 function addHead() {
-  const randomE = Math.floor(Math.random() * 8);
-
-  // select random neck
-  let imgNeck = new Image();
-  imgNeck.src = neckImages[randomE].src;
+  const colorSetACount = colorSetA.length;
+  const randomColorA = Math.floor(Math.random() * colorSetACount);
 
   // get head shape that is clicked on
   const img = new Image();
-  img.src = event.target.src;
+  img.src = this.event.target.src;
 
-  headFile = img.src;
+  // get default neck shape
+  const imgNeck = new Image();
+  imgNeck.src = dir + '/media/neck/neck-001.svg';
 
-  // if (isHead != headFile) {
-    // draw head if there's no or different head
-    img.onload = function() {
-      const canvas = document.getElementById("headCanvas");
-      const ctx = canvas.getContext("2d");
-      ctx.clearRect(0, 0, avatarW, avatarH);
-      ctx.drawImage(imgNeck, 0, 0, avatarW, avatarH);
-      ctx.drawImage(img, 0, 0, avatarW, avatarH);
-      mergeCanvases();
-      isHead = headFile;
-    }
-    setActiveClass('headElement', 'headElementActive');
-  // }
+  img.onload = function() {
+    const canvas = document.getElementById("headCanvas");
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, avatarW, avatarH);
+
+    // draw head
+    ctx.globalCompositeOperation = "source-over";
+    ctx.drawImage(img, 0, 0, avatarW, avatarH);
+
+    // color head
+    ctx.globalCompositeOperation = "source-atop";
+    ctx.fillStyle = colorSetA[randomColorA];
+    ctx.fillRect(0, 0, avatarW, avatarH);
+
+    // draw neck
+    ctx.globalCompositeOperation = "destination-over";
+    ctx.drawImage(imgNeck, 0, 0, avatarW, avatarH);
+
+    mergeCanvases();
+  };
+  setActiveClass('headElement', 'headElementActive');
 }
 
 /**
@@ -67,9 +97,17 @@ function addHead() {
  */
 function addEyes() {
   const img = new Image();
-  img.src = event.target.src;
-  eyesFile = img.src;
-  if (isEyes != eyesFile) {
+  img.src = this.event.target.src;
+  const eyesFile = img.src;
+  if (isEyes === eyesFile) {
+    // remove eyes if same eyes is on canvas
+    const canvas = document.getElementById("eyesCanvas");
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, avatarW, avatarH);
+    mergeCanvases();
+    isEyes = null;
+    this.event.target.setAttribute("class", "eyesElement");
+  } else {
     // draw eyes if there's no or different eyes
     img.onload = function() {
       const canvas = document.getElementById("eyesCanvas");
@@ -81,16 +119,8 @@ function addEyes() {
       ctx.drawImage(img, 0, offsetV, avatarW, avatarH);
       mergeCanvases();
       isEyes = eyesFile;
-    }
+    };
     setActiveClass('eyesElement', 'eyesElementActive');
-  } else {
-    // remove eyes if same eyes is on canvas
-    const canvas = document.getElementById("eyesCanvas");
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, avatarW, avatarH);
-    mergeCanvases();
-    isEyes = null;
-    event.target.setAttribute("class", "eyesElement");
   }
 }
 
@@ -99,9 +129,17 @@ function addEyes() {
  */
 function addNose() {
   const img = new Image();
-  img.src = event.target.src;
-  noseFile = img.src;
-  if (isNose != noseFile) {
+  img.src = this.event.target.src;
+  const noseFile = img.src;
+  if (isNose === noseFile) {
+    // remove nose if same nose is on canvas
+    const canvas = document.getElementById("noseCanvas");
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, avatarW, avatarH);
+    mergeCanvases();
+    isNose = null;
+    this.event.target.setAttribute("class", "noseElement");
+  } else {
     // draw nose if there's no or different nose
     img.onload = function() {
       const canvas = document.getElementById("noseCanvas");
@@ -113,16 +151,8 @@ function addNose() {
       ctx.drawImage(img, 0, offsetV, avatarW, avatarH);
       mergeCanvases();
       isNose = noseFile;
-    }
+    };
     setActiveClass('noseElement', 'noseElementActive');
-  } else {
-    // remove nose if same nose is on canvas
-    const canvas = document.getElementById("noseCanvas");
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, avatarW, avatarH);
-    mergeCanvases();
-    isNose = null;
-    event.target.setAttribute("class", "noseElement");
   }
 }
 
@@ -131,9 +161,17 @@ function addNose() {
  */
 function addMouth() {
   const img = new Image();
-  img.src = event.target.src;
-  mouthFile = img.src;
-  if (isMouth != mouthFile) {
+  img.src = this.event.target.src;
+  const mouthFile = img.src;
+  if (isMouth === mouthFile) {
+    // remove mouth if same mouth is on canvas
+    const canvas = document.getElementById("mouthCanvas");
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, avatarW, avatarH);
+    mergeCanvases();
+    isMouth = null;
+    this.event.target.setAttribute("class", "mouthElement");
+  } else {
     // draw mouth if there's no or different mouth
     img.onload = function() {
       const canvas = document.getElementById("mouthCanvas");
@@ -145,23 +183,15 @@ function addMouth() {
       ctx.drawImage(img, 0, offsetV, avatarW, avatarH);
       mergeCanvases();
       isMouth = mouthFile;
-    }
+    };
     setActiveClass('mouthElement', 'mouthElementActive');
-  } else {
-    // remove mouth if same mouth is on canvas
-    const canvas = document.getElementById("mouthCanvas");
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, avatarW, avatarH);
-    mergeCanvases();
-    isMouth = null;
-    event.target.setAttribute("class", "mouthElement");
   }
 }
 
 /**
  * merge all canvases into one, ready for downloading
  */
-function mergeCanvases(){
+function mergeCanvases() {
   const headC = document.getElementById('headCanvas');
   const eyesC = document.getElementById('eyesCanvas');
   const noseC = document.getElementById('noseCanvas');
@@ -182,11 +212,12 @@ function mergeCanvases(){
 /**
  * download png of merged canvas
  */
-function download(){
+function download() {
   const download = document.getElementById("download");
   const canvas = document.getElementById('mergedCanvas');
-  const image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-  download.setAttribute("href", image);
+  const imagepng = canvas.toDataURL("image/png");
+  const imagestream = imagepng.replace("image/png", "image/octet-stream");
+  download.setAttribute("href", imagestream);
 }
 
 /**
@@ -197,7 +228,7 @@ function removeHead() {
   const ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, avatarW, avatarH);
   mergeCanvases();
-  event.target.setAttribute("class", "headElement");
+  this.event.target.setAttribute("class", "headElement");
 }
 
 /**
@@ -232,14 +263,16 @@ function removeMouth() {
 
 /**
  * set active class and remove it from previous element
+ * @param {string} inactiveClass inactive class name
+ * @param {string} activeClass active class name
  */
 function setActiveClass(inactiveClass, activeClass) {
   // remove active class from other active element
   const elements = document.getElementsByClassName(activeClass);
   const requiredElement = elements[0];
-  if(requiredElement) {
+  if (requiredElement) {
     requiredElement.setAttribute("class", inactiveClass);
   }
   // set active class
-  event.target.setAttribute("class", activeClass);
+  this.event.target.setAttribute("class", activeClass);
 }
